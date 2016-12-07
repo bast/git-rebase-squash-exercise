23736bacf6f3804f83db8aa429c0e4011a7fc478
Radovan Bast <bast@users.noreply.github.com>
2016-12-07 17:23:43 +0100

    forgot to add a file.
    startign to work on it (commit with a typo)
    
    another incomplete commit
    
    completed the haiku
    
    fix a copy-paste error

---
+++ haiku.py
@@ -0,0 +1,8 @@
+def get_haiku():
+
+    return '''On a branch ...
+                  by Kobayashi Issa
+
+              On a branch
+              floating downriver
+              a cricket, singing.'''
