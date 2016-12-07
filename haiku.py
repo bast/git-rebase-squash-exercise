17ba6254a8a415081851055cf9e32c4a0fc19f1a
Radovan Bast <bast@users.noreply.github.com>
2016-12-07 17:22:33 +0100

    starting working on the haiku
    forgot to add a file
    
    startign to work on it (commit with a typo)
    
    another incomplete commit
    
    completed the haiku

---
+++ haiku.py
@@ -0,0 +1,9 @@
+def get_haiku():
+
+    return '''On a branch ...
+                  by Kobayashi Issa
+
+              On a branch
+              floating downriver
+              floating downriver
+              a cricket, singing.'''
